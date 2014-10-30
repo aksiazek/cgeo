094c6906ad6e1f6881d11d7db5a6343bc982b937
aksiazek <alexander.ksiazek@gmail.com>
2014-10-30 13:47:30 +0100

    graham and jarvis algorithms

--- lab2/convex_hull.cpp
+++ lab2/convex_hull.cpp
@@ -1,54 +1,151 @@
 #include <cmath>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+#include <stack>
 
-#include "predicates.h"
 #include "gogui/libraries/cpp/gogui.hpp"
 
 using namespace std;
 
-FILE* file;
+struct point {
+		double x, y;
+};
 
+point first_element;
 
-int classify(double val) {
-	
-	if(val < 0.0) {
-        return  0;
-	} else if (val == 0.0) {
-        return  1;
-    } else
-        return 2;
+double manual_det1(point a, point b, point c) {
+	return a.x*b.y + b.x*c.y + c.x*a.y - b.y*c.x - c.y*a.x - a.y*b.x;
 }
 
-double manual_det1(double* a, double* b, double* c) {
-	return a[0]*b[1] + b[0]*c[1] + c[0]*a[1] - b[1]*c[0] - c[1]*a[0] - a[1]*b[0];
+double ccw(point p1, point p2, point p3) {
+    return (p1.x - p3.x)*(p2.y - p3.y) - (p1.y - p3.y)*(p2.x - p3.x);
 }
 
-double manual_det2(double* a, double* b, double* c) {
-	return (a[0] - c[0])*(b[1]-c[1]) - (a[1]-c[1])*(b[0]-c[0]);
+bool order_by_angle(point b, point c) {
+	return ((first_element.x - c.x)*(b.y-c.y) - (first_element.y-c.y)*(b.x-c.x)) < 0;
 }
 
-void load_dataset(gogui::vector<gogui::Point>* cloud, const char* filename) {
-	file = fopen(filename, "r");
+void load_dataset(vector<point>& cloud, const char* filename) {
+	FILE* file = fopen(filename, "r");
 	if(file != NULL) {
-		while(!eof) {
-			
-			double point[2];
-			fscanf(file, "%lf %lf", &point[0], &point[1]);
-			
-			gogui::Point p(point[0],point[1]);
-			cloud->push_back(p);
-            
+		point p;
+		while(!feof(file)) {
+			fscanf(file, "%lf %lf", &p.x, &p.y);
+			cloud.push_back(p);
 		}
 		fclose(file);
 	}
 }
 
-int main() {
+bool order_by_y_x(point a, point b) { 
+	if (a.y != b.y)
+		return a.y < b.y; 
+	else
+		return a.x < b.x; 
+}
 
-	gogui::vector<gogui::Point> cloud;
-	load_dataset(&cloud, "a.dat");
+void graham_hull(vector<point>& cloud) {
+	vector<point>::size_type N = cloud.size();
+	vector<point>::iterator first_element_y = min_element(cloud.begin(), cloud.end(), order_by_y_x);
+	swap(*cloud.begin(), *first_element_y);
+	first_element = *cloud.begin();
 	
+	printf("%lf\n", first_element.y);
+	
+	sort(cloud.begin()+1, cloud.end(), order_by_angle);
+	
+	/*for (unsigned i = 0; i < N; i++) 
+		printf("%lf %lf\n", cloud[i].x, cloud[i].y);*/
+	
+	stack<point> hull;
+	hull.push(cloud[0]); hull.push(cloud[1]); hull.push(cloud[2]);
+	
+	unsigned i = 3, hull_size = 3;
+	while (i < N) {
+		point stack_top = hull.top();
+		hull.pop();
+		if (ccw(hull.top(), stack_top, cloud[i]) < 0) {
+			hull_size++;
+			hull.push(stack_top);
+			hull.push(cloud[i]);
+			i++;
+		} else {
+			// hull.pop();
+			hull_size--;	
+		}
+	}
+	
+	point first = hull.top();
+	for (unsigned i = 0; i < hull_size; i++) {
+		point p = hull.top();
+		hull.pop();
+		printf("%lf %lf\n", p.x, p.y);
+	}
+	printf("%lf %lf\n", first.x, first.y);
+}
+
+void jarvis_hull(vector<point>& cloud) {
+	
+	stack<point> hull;
+	
+	vector<point>::size_type N = cloud.size();
+	vector<point>::iterator first_element_y = min_element(cloud.begin(), cloud.end(), order_by_y_x);
+	
+	vector<point>::iterator i = first_element_y;
+	
+	hull.push(*i);
+	printf("push y_min elem %lf %lf\n", i->x, i->y);
+	
+	do {
+		bool any = false;
+		vector<point>::iterator k;
+		
+		for(vector<point>::iterator j = cloud.begin(); j < cloud.end(); j++) {
+			if(j == i) continue;
+			if(!any) {
+				k = j;
+				//printf("first kandidate %lf %lf\n", k->x, k->y);
+				any = true;
+				continue;
+			}
+			
+			if (ccw(hull.top(), *k, *j) > 0) {
+				k = j;
+				//printf("new kandidate %lf %lf\n", k->x, k->y);
+			}
+			
+		}
+		hull.push(*k);
+		//printf("push kandidate %lf %lf\n", k->x, k->y);
+		i = k;
+		
+	} while (i != first_element_y);
+	
+	//point first = hull.top();
+	while(!hull.empty()) {
+		point p = hull.top();
+		hull.pop();
+		printf("%lf %lf\n", p.x, p.y);
+	}
+	//printf("%lf %lf\n", first.x, first.y);
+}
+
+int main(int argc, char* argv[]) {
+
+	if(argc != 2) {
+			printf("usage: %s [dataset]\n", argv[0]);
+			exit(-1);
+	}
+	vector<point> cloud;
+	load_dataset(cloud, argv[1]);
+	
+	//graham_hull(cloud);
+	
+	jarvis_hull(cloud);
 	
 	return 0;
 }
--- lab2/plot.gnu
+++ lab2/plot.gnu
@@ -4,9 +4,8 @@ set ylabel "y"
 #set palette model RGB defined (0 "blue", 1 "red", 2 "green")
 #unset colorbox
 
-#plot "a.dat" using 1:2 with points title 'podpunkt a' 
-#plot "b.dat" using 1:2 with points title 'podpunkt b' 
-#plot "c.dat" using 1:2 with points title 'podpunkt c' 
-plot "d.dat" using 1:2 with points title 'podpunkt d' 
+plot filename using 1:2 with points title 'podpunkt' 
+if(filename eq "hull.dat") plot "hull.dat" using 1:2 with linespoints title 'otoczka' 
 pause -1
+
 
